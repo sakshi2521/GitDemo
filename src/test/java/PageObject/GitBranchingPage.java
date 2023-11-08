e256fe676d2f0016826b5128143eec85165179f1
Sakshi Vikhe <svikhe767@gmail.com>
2023-11-08 12:45:57 +0530

    user 2 changes to merge pages and master

--- src/test/java/PageObject/GitBranchingPage.java
+++ src/test/java/PageObject/GitBranchingPage.java
@@ -3,5 +3,7 @@ package PageObject;
 public class GitBranchingPage {
 public static void main(String[] args) {
 	System.out.println("branch Pages");
+	
+	System.out.println("merging pages and master");
 }
 }
